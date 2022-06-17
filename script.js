23d0de3042e962e8e9a464f281ea206bbb6f2b04
marise-san <marisemrtns@gmail.com>
2022-06-17 14:08:18 -0300

    Add a button that changes the canvas size

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function createGrid(size) {
     const container = document.querySelector('.container');
+
     container.style.setProperty('--grid-rows', size);
     container.style.setProperty('--grid-cols', size);
-    for (g = 0; g < (size * size); g++) {
+    for (let g = 0; g < (size * size); g++) {
         let grid = document.createElement('div');
         container.appendChild(grid).className = 'grid-item';
     }
 }
 
-createGrid(16);
+createGrid(16); // default grid size
 
 const changeColor = (e) => e.target.classList.add('color');
 const items = document.querySelectorAll('.grid-item');
 items.forEach(item => item.addEventListener('mouseover', changeColor));
+
+function changeSize(inputSize){
+    createGrid(inputSize);
+}
