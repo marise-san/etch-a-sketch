afed1a9fc5a12f4d9c48fa4b6eee84ea6cb22a73
marise-san <marisemrtns@gmail.com>
2022-06-17 14:13:36 -0300

    Fix pen not working after canvas resize

--- script.js
+++ script.js
@@ -5,16 +5,13 @@ function createGrid(size) {
     container.style.setProperty('--grid-cols', size);
     for (let g = 0; g < (size * size); g++) {
         let grid = document.createElement('div');
+        grid.addEventListener('mouseover', function(e) {
+            e.target.classList.add('color');
+        });
         container.appendChild(grid).className = 'grid-item';
     }
 }
 
 createGrid(16); // default grid size
 
-const changeColor = (e) => e.target.classList.add('color');
-const items = document.querySelectorAll('.grid-item');
-items.forEach(item => item.addEventListener('mouseover', changeColor));
-
-function changeSize(inputSize){
-    createGrid(inputSize);
-}
+const changeSize = (inputSize) => createGrid(inputSize);
